bc8c9d257c82d871b8b5d224522482e3024f68e3
Veerala-Rupa <veeralarupa@gmail.com>
2024-06-07 21:06:09 +0530

    Bill management

---
+++ Bill Managment.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+root = Tk()
+root.geometry("1000x500")
+root.title("Bill Management")
+root.resizable(False,False)
+
+Label(text="BILL MANAGEMENT",)
+
+root.mainloop()
